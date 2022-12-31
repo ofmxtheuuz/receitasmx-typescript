c0f86c9fb73a07f53336b8420735abe856f90cd6
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-12-31 13:20:48 -0300

    Update default.ts

--- config/default.ts
+++ config/default.ts
@@ -1,6 +1,7 @@
 let user = process.env.DB_USER
 let password = process.env.DB_PASS
 
+// config
 export default {
     port: 3000,
     dbUri: `mongodb+srv://${user}:${password}@cluster0.dqcfam1.mongodb.net/ReceitasMx?retryWrites=true&w=majority`
