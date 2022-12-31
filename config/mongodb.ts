d17fba2febfa6960cbc7908f4103c413b8ff0cd7
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-12-31 13:20:33 -0300

    Update mongodb.ts

--- config/mongodb.ts
+++ config/mongodb.ts
@@ -3,8 +3,10 @@ import config from "config"
 import * as log from "../src/helpers/message"
 
 async function connect() {
+    // get db uri
     const dbUri = config.get<string>("dbUri")
     try {
+        // async
         await mongoose.connect(dbUri)
         log.database("Banco de dados conectado com sucesso!");
     } catch (err: any) {
